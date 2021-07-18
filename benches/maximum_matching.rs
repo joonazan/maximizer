0f28f4be8b00ed02ffa027168a6dfcaddda0dd12
Joonatan Saarhelo <joon.saar@gmail.com>
2021-07-18 23:05:21 +0300

    add bigger maximum matching benchmark

--- benches/maximum_matching.rs
+++ benches/maximum_matching.rs
@@ -1,7 +1,7 @@
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 use maximizer::line_superiority::{maximum_matching, maximum_matching_simple};
 
-pub fn criterion_benchmark(c: &mut Criterion) {
+pub fn maximum_matching_4(c: &mut Criterion) {
     let neighbors: [&[usize]; 4] = [&[0, 2], &[0, 1], &[3, 0], &[0, 3, 2]];
 
     c.bench_function("Maximum matching 4", |b| {
@@ -12,5 +12,26 @@ pub fn criterion_benchmark(c: &mut Criterion) {
     });
 }
 
-criterion_group!(benches, criterion_benchmark);
+pub fn maximum_matching_9(c: &mut Criterion) {
+    let neighbors: [&[usize]; 9] = [
+        &[1, 4, 5, 6, 8],
+        &[5, 6, 7],
+        &[0, 3, 4, 5, 6, 7, 8],
+        &[2, 3, 4, 7],
+        &[0, 1, 2, 5, 8],
+        &[2, 4, 5, 6],
+        &[0, 1, 2, 4, 5, 6, 7, 8],
+        &[0, 1, 2, 5, 7, 8],
+        &[0, 1, 2, 3, 7],
+    ];
+
+    c.bench_function("Maximum matching 9", |b| {
+        b.iter(|| maximum_matching(black_box(&neighbors)))
+    });
+    c.bench_function("Maximum matching simple 9", |b| {
+        b.iter(|| maximum_matching_simple(black_box(&neighbors)))
+    });
+}
+
+criterion_group!(benches, maximum_matching_4, maximum_matching_9);
 criterion_main!(benches);
